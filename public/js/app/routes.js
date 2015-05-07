6d1cd67f51f23d2663618f224fe1c99d38cadd5f
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-05-06 23:31:16 -0300

    Criação do estado base e preparação paralogin com ui-router

--- public/js/app/routes.js
+++ public/js/app/routes.js
@@ -1,11 +1,28 @@
 var fs = require('fs');
 
 module.exports = function ($stateProvider,$locationProvider,$urlRouterProvider) {
-	$urlRouterProvider.otherwise('/perfil');
+	$urlRouterProvider.otherwise('/home');
 	$locationProvider.html5Mode(false);
 	$stateProvider
-		.state('home', {
-			url:'/perfil',
+		.state('main', {
+			abstract: true,
+			views: {
+				'master': {
+					template: fs.readFileSync(__dirname + '/perfil/templates/master.html')  
+				}
+			}
+		})
+		.state('login', {
+			url: '/login',
+			views: {
+				'master': {
+					// controller: 'vimAController',
+					template: "<h1><a ui-sref='main.home'> Entrar no sistema</a> </h1>"
+				},
+			}
+		})
+		.state('main.home', {
+			url: '/home',
 			views: {
 				'aside': {
 					// controller: 'vimAController',
