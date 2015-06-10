be4778e16094654f22cbf66416a0c1e041a23ac0
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-06-10 16:26:54 -0300

    Ajustes no menu

--- public/js/app/core/controllers/headerController.js
+++ public/js/app/core/controllers/headerController.js
@@ -1,5 +1,14 @@
 module.exports = function ($scope, coreEventsService, authModelService) {
 	var coreEvents = $scope.coreEventse = coreEventsService;
+	$scope.plans = [
+		"12/04/15 - 12/05/15",
+		"13/04/15 - 12/05/15",
+		"14/04/15 - 12/05/15",
+		"19/04/15 - 12/05/15",
+		"18/04/15 - 12/05/15",
+		"15/04/15 - 12/05/15",
+		"16/04/15 - 12/05/15"
+	];
 	$scope.logout = function () {
 		authModelService.logout();
 	};
