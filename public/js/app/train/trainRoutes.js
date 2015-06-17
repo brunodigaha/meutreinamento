ce5c77b763beded600c32b241aa29b2d06a1a74c
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-06-17 19:48:37 -0300

    Ajustes nas sombras

--- public/js/app/train/trainRoutes.js
+++ public/js/app/train/trainRoutes.js
@@ -16,9 +16,10 @@ module.exports = {
 						$scope.showGridBottomSheet = function($event) {
 							$mdBottomSheet.show({
 								// template: fs.readFileSync(__dirname + '/templates/TrainingGridBottomSheet.html'),
-								template: '<md-bottom-sheet>Hello!</md-bottom-sheet>',
+								template: '<md-bottom-sheet class="md-grid"><md-list><md-list-item><md-button><ng-md-icon icon="launch" style="fill:gray" size="32"><div> Visualizar </div> </ng-md-icon> </md-button></md-list-item></md-list></md-bottom-sheet>',
+								// template: '<md-bottom-sheet class="md-grid"> <md-list> <md-list-item ng-repeat="item in items"> <md-button class="md-grid-item-content" ng-click="listItemClick($index)"> <ng-md-icon icon="launch" style="fill:gray" size="22"></md-icon> <div class="md-grid-text">{{item.name}}</div></md-button> </md-list-item> </md-list></md-bottom-sheet>',
 								controller: 'TrainingGridBottomSheetCtrl',
-								// parent: angular.element(document.getElementById('aqui')),
+								parent: angular.element(document.getElementById('training')),
 								targetEvent: $event
 							}).then(function(clickedItem) {
 								// alert(clickedItem.name + ' clicked!');
