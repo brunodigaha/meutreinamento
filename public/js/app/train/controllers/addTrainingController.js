13e69495cbaa4a93ce1d85f689a3c2b164f4d2a6
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-06-24 16:35:18 -0300

    Criação inicial do campo autocomplete para exercicios em addtraining

--- public/js/app/train/controllers/addTrainingController.js
+++ public/js/app/train/controllers/addTrainingController.js
@@ -1,5 +1,92 @@
 var fs = require('fs');
-module.exports = function($scope, coreEventsService,$state,$stateParams,$mdDialog) {
+module.exports = function($scope,$timeout,$q,$log, coreEventsService,$state,$stateParams,$mdDialog) {
+
+
+
+    var self = $scope;
+
+    self.simulateQuery = false;
+    self.isDisabled    = false;
+
+    self.repos         = loadAll();
+    self.querySearch   = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+
+    // ******************************
+    // Internal methods
+    // ******************************
+
+    /**
+     * Search for repos... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch (query) {
+      var results = query ? self.repos.filter( createFilterFor(query) ) : self.repos,
+          deferred;
+      if (self.simulateQuery) {
+        deferred = $q.defer();
+        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+        return deferred.promise;
+      } else {
+        return results;
+      }
+    }
+
+    function searchTextChange(text) {
+      $log.info('Text changed to ' + text);
+    }
+
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
+    }
+
+    /**
+     * Build `components` list of key/value pairs
+     */
+    function loadAll() {
+      var repos = [
+        {
+          'name'      : 'Supino Reto'
+        },
+        {
+          'name'      : 'Supino Inclinado'
+        },
+        {
+          'name'      : 'Crucifixo'
+        },
+        {
+          'name'      : 'Flay'
+        },
+        {
+          'name'      : 'Biceps Testa',
+          'url'       : 'https://github.com/angular/material-start',
+          'watchers'  : '81',
+          'forks'     : '303',
+        }
+      ];
+      return repos.map( function (repo) {
+        repo.value = repo.name.toLowerCase();
+        return repo;
+      });
+    }
+
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(item) {
+        return (item.value.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+
+
+
+
+
 	$scope.params = $stateParams;
 	$scope.state = $state.current;
 	$scope.plans = [
