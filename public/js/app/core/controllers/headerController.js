06d5d8ecc8930c261d881230b21a3b8749a0ea79
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-06-29 20:56:10 -0300

    Inserção do modal alterar plano de treinamento

--- public/js/app/core/controllers/headerController.js
+++ public/js/app/core/controllers/headerController.js
@@ -1,4 +1,5 @@
-module.exports = function ($scope, coreEventsService, authModelService) {
+var fs = require('fs');
+module.exports = function ($scope,$mdDialog, coreEventsService, authModelService) {
 	var coreEvents = $scope.coreEventse = coreEventsService;
 	$scope.plans = [
 		"12/04/15 - 12/05/15",
@@ -15,4 +16,19 @@ module.exports = function ($scope, coreEventsService, authModelService) {
 	$scope.toggle_options = function () {
 		coreEventsService.toggle_options();
 	};
+	$scope.edit_plan = function(ev) {
+		$mdDialog.show({
+			controller: 'editPlanDialogController',
+			// templateUrl: 'dialog1.tmpl.html',
+			template: fs.readFileSync(__dirname + '/../templates/editPlanDialogTemplate.html'),
+			parent: angular.element(document.body),
+			targetEvent: ev,
+		})
+		.then(function(answer) {
+			$scope.alert = 'You said the information was "' + answer + '".';
+		}, function() {
+			$scope.alert = 'You cancelled the dialog.';
+		});
+	};
+
 };
