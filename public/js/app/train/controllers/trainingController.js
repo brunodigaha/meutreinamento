361f68f9254579daf368d3c6ad0666c171ec3342
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-07-03 15:32:51 -0300

    Remoção do bottonsheet  e suas dependencias, e ajuste em training template

--- public/js/app/train/controllers/trainingController.js
+++ public/js/app/train/controllers/trainingController.js
@@ -1,17 +1,3 @@
-module.exports = function($scope,$timeout,$q,$log, coreEventsService,$state,$stateParams,$mdDialog,$mdBottomSheet) {
-	$scope.showGridBottomSheet = function($event) {
-		$mdBottomSheet.show({
-			// template: fs.readFileSync(__dirname + '/templates/TrainingGridBottomSheet.html'),
-			template: '<md-bottom-sheet class="md-grid"><md-list><md-list-item><md-button><ng-md-icon icon="launch" style="fill:gray" size="32"><div> Visualizar </div> </ng-md-icon> </md-button></md-list-item></md-list></md-bottom-sheet>',
-			// template: '<md-bottom-sheet class="md-grid"> <md-list> <md-list-item ng-repeat="item in items"> <md-button class="md-grid-item-content" ng-click="listItemClick($index)"> <ng-md-icon icon="launch" style="fill:gray" size="22"></md-icon> <div class="md-grid-text">{{item.name}}</div></md-button> </md-list-item> </md-list></md-bottom-sheet>',
-			controller: 'TrainingGridBottomSheetCtrl',
-			parent: angular.element(document.getElementById('training')),
-			targetEvent: $event
-		}).then(function(clickedItem) {
-			// alert(clickedItem.name + ' clicked!');
-			console.log("teste");
-		});
-	};
-
+module.exports = function($scope) {
 
 };
--- public/js/app/train/index.js
+++ public/js/app/train/index.js
@@ -1,5 +1,4 @@
 module.exports = angular.module('train',[])
-		.controller('TrainingGridBottomSheetCtrl', require('./controllers/TrainingGridBottomSheetCtrl.js'))
 		.controller('listTrainingController', require('./controllers/listTrainingController.js'))
 		.controller('trainingController', require('./controllers/trainingController.js'))
 		.controller('addTrainingController', require('./controllers/addTrainingController.js'))
