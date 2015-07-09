3d44f6091e59fb544043d1a863f70c19e9cb5f4b
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-07-09 14:46:53 -0300

    Criação da diretiva ds-info para adicionar informações

--- public/js/app/appDirectives/index.js
+++ public/js/app/appDirectives/index.js
@@ -1,2 +1,3 @@
 module.exports = angular.module('appDirectives',[])
-		.directive('dsTraining', require('./directives/dsTrainingDirective.js'));
+		.directive('dsTraining', require('./directives/dsTrainingDirective.js'))
+		.directive('dsInfo', require('./directives/dsInfoDirective.js'));
