48f9505f802ad8a5a7b64e7d62480e4e5db1171d
Bruno Alexandre <bruce_alex2006@hotmail.com>
2015-07-16 19:42:51 -0300

    Inserção inicial do template em planos

--- public/js/app/appDirectives/directives/dsPlanDirective.js
+++ public/js/app/appDirectives/directives/dsPlanDirective.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 module.exports = function() {
 	return{
 		template: fs.readFileSync(__dirname + '/../templates/dsPlanTemplateDIrective.html'),
-		// transclude:true
+		transclude:true
 		// scope: {
 		// 	title: "@",
 		// 	content: "@"
